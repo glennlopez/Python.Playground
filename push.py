956f8fa52197e003b4ff35f1cf8a10d642642ea1
glennlopez <glennlopez@gmail.com>
2014-08-06 14:43:11 -0500

    update pydate

--- push.py
+++ push.py
@@ -3,13 +3,13 @@ import subprocess
 import os 
 
 # use these for coloring your text
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
+class colors:
+    PURPL = '\033[95m'
+    BLUE = '\033[94m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    RED = '\033[91m'
+    WHITE = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
@@ -19,12 +19,12 @@ def cmd(cmd):
 
 # commands to excecute 
 cmd('clear')
-print bcolors.BOLD + "Github Update Script" + bcolors.ENDC
-comment = raw_input(bcolors.GREEN + "Type your update comment: " + bcolors.ENDC)				
+print colors.BOLD + "Github Update Script" + colors.WHITE
+comment = raw_input(colors.GREEN + "Type your update comment: " + colors.WHITE)				
 cmd('git add *') 	#updates changes made inside files
 cmd('git add -u') 	#updated deleted files			
 cmd('git status')	#displays changes to be pushed to github
 cmd("git commit -m '"+comment+"'")
-cmd('git push')
+cmd('git push')		#push changes to github
 print 
-print bcolors.YELLOW + "Update Complete!" + bcolors.ENDC
+print colors.YELLOW + "Update Complete!" + colors.WHITE
