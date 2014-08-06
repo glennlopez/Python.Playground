08dc634f8069f71b0e1bf75dbd4a45a78267bc50
glennlopez <glennlopez@gmail.com>
2014-08-06 13:36:01 -0500

    test update

--- push.py
+++ push.py
@@ -2,12 +2,20 @@
 import subprocess
 import os 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
 
 def cmd(cmd):
 	os.system(cmd)
 
 cmd('clear')
-print 'Updating...'
+print 'Pushing commits to Github...'
 comment = raw_input("Type git-commit comment: ")				
 cmd('git add *')			
 cmd('git status')
@@ -15,4 +23,5 @@ cmd('git diff')
 cmd("git commit -m '"+comment+"'")
 cmd('git push')
 print 
-cmd('echo "\e[0;31mPush Script Complete!"')
+cmd('echo Push Script Complete!')
+print bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC
