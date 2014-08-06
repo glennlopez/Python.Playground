007d5fe97e3f4cd82c94a1f4b61a32ac597a0801
glennlopez <glennlopez@gmail.com>
2014-08-06 12:52:09 -0500

    testupdate

---
+++ push.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import subprocess
+import os 
+
+def cmd(cmd):
+	os.system(cmd)
+		#git add *
+		#git status -s
+		#git commit -m '<comment goes here>'
+		#git push
+comment = raw_input("Type Comment: ")
+cmd('clear')				#clears terminal
+cmd('git add *')			#adds changes
+cmd('git status')
+cmd('git diff')
+cmd("git commit -m '"+comment+"'")
+cmd('git push')
