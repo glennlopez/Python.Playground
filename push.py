516240353495cd99e17798b0f5c4a8876f19a6e7
glennlopez <glennlopez@gmail.com>
2014-08-06 12:56:09 -0500

    test update 1

--- push.py
+++ push.py
@@ -4,15 +4,13 @@ import os
 
 def cmd(cmd):
 	os.system(cmd)
-		#git add *
-		#git status -s
-		#git commit -m '<comment goes here>'
-		#git push
+
 comment = raw_input("Type Comment: ")
-cmd('clear')				#clears terminal
-cmd('git add *')			#adds changes
+cmd('clear')				
+cmd('git add *')			
 cmd('git status')
 cmd('git diff')
 cmd("git commit -m '"+comment+"'")
 cmd('git push')
+print 
 cmd('echo Push Script Complete!')
