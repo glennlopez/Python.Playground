0f16f7acf2a98e9f591a9c5de5ce435efb73a84b
glennlopez <glennlopez@gmail.com>
2014-08-23 22:24:52 -0500

    updated macchanger

--- TOOLS/macspoof.py
+++ TOOLS/macspoof.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 '''
-This script automates the whole proccess of spoofing mac address for
-recon or getting unblocked/free wifi
+This script automates spoofing of mac address
+source: github.com/glennlopez
 '''
+
+# LIBRARIES
 import subprocess			# Subprocess for Shell CMD
 import os 					# OS Library
 
@@ -23,44 +25,32 @@ def cmd(cmd):
 
 
 cmd('clear')
-
 print colors.BOLD + 'Starting Monitor mode...' + colors.WHITE
-print colors.YELLOW + 'be patient, this will take some time' + colors.WHITE
+print colors.YELLOW + '[..] be patient, this will take some time' + colors.WHITE
 cmd('sleep 2')
-
-
 cmd('ifconfig wlan0 up')
-print colors.GREEN+'All Done!'+colors.WHITE
 cmd('airmon-ng start wlan0')
 cmd('clear')
-print colors.GREEN+ 'PRESS [CTRL] + [C] to stop Monitor Mode...' + colors.WHITE
+print colors.GREEN+ '[..] PRESS [CTRL] + [C] to stop Monitor Mode' + colors.WHITE
 print 
 print colors.BOLD+'[HACK TIP]'+colors.WHITE
-print 'In Monitor mode, wait till you get enough frames.'
-print 'Then copy that mac address and use it as your own to fool payed intern.'
+print '- Wait till you get enough frames.'
+print '- Spoof your Mac Address to bypass captive portals.'
+print '- Change your Mac Address to gain extra complimentary Wi-Fi'
 cmd('sleep 4')
-
 cmd('airodump-ng mon0')
 
 #open new terminal window with specific monitor
 #mon_ap = raw_input(colors.PURPL+'Target Station BSSID Mac Address: '+colors.WHITE)
 #cmd('gnome-terminal -e sh -c "airmon-ng --bssid "+mon_ap+"; bash"')
 
-
-print colors.YELLOW+'turning off wlan0 and mon0'+colors.WHITE
+print colors.YELLOW+'[+] wlan0 and mon0 are now off (Wi-Fi is down)'+colors.WHITE
 cmd('ifconfig wlan0 down')
 cmd('ifconfig mon0 down')
-
-
-mac = raw_input(colors.RED+'Change MAC Address to: '+colors.WHITE)
-
-
+mac = raw_input(colors.RED+'[?] Change MAC Address to: '+colors.WHITE)
 cmd('macchanger -m '+mac+' wlan0')
-print colors.YELLOW+'waking up wlan0...'+colors.WHITE
+print colors.YELLOW+'[+] Wi-Fi turned back on'+colors.WHITE
 cmd('sleep 2.5')
 cmd('ifconfig wlan0 up')
-cmd('ifconfig mon1 down')
-cmd('ifconfig mon2 down')
-cmd('ifconfig mon3 down')
-print colors.GREEN+'All Done! - Turn on/off Wi-Fi'+colors.WHITE
+print colors.GREEN+'[!] All Done.'+colors.WHITE
 cmd('ifconfig | grep wlan0')
