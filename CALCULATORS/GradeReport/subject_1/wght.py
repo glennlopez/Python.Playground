c61b5c11b0166fadf361a5ca51eaaab5f5dc7fd4
Glenn Lopez <glennlopez@gmail.com>
2019-01-17 12:51:03 -0600

    removed old crap

--- CALCULATORS/GradeReport/subject_1/wght.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-'''
-You can define the weight of each item as per course, below.
-'''
-
-hmw_wght = 0.1		#homework weight
-qzz_wght = 0.3		#quizzes weight
-tst_wght = 0.6		#tests wieght
