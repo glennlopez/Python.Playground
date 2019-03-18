7722d589bd843d714d9a23c80e046e425c32e83f
Glenn Lopez <glennlopez@gmail.com>
2019-03-18 01:32:03 -0500

    answered

--- Lessons/Introduction/2 - Data Types & Operators/tiler_calculation.py
+++ Lessons/Introduction/2 - Data Types & Operators/tiler_calculation.py
@@ -9,7 +9,7 @@ You buy 17 packages of tiles containing 6 tiles each. How many tiles will be lef
 """
 
 # Fill this in with an expression that calculates how many tiles are needed.
-print()
+print( (9 * 7) + (5 * 7) )
 
 # Fill this in with an expression that calculates how many tiles will be left over.
-print()
+print( (17 * 6) - ((9 * 7) + (5 * 7)) )
