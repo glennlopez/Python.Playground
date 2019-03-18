a1446009e4d62526ea8ce281e11f6a47db1a4602
Glenn Lopez <glennlopez@gmail.com>
2019-03-18 14:24:52 -0500

    added new quiz

--- Lessons/Introduction/2 - Data Types & Operators/rio_or_sanfran.py
+++ Lessons/Introduction/2 - Data Types & Operators/rio_or_sanfran.py
@@ -13,3 +13,4 @@ san_francisco_pop_density = sf_population/sf_area
 rio_de_janeiro_pop_density = rio_population/rio_area
 
 # Write code that prints True if San Francisco is denser than Rio, and False otherwise
+print(san_francisco_pop_density > rio_de_janeiro_pop_density)
