0bc287e04ad255ef89802ec9d1c060066f7202b2
Glenn Lopez <glennlopez@gmail.com>
2019-03-18 14:27:38 -0500

    solved

--- Lessons/Introduction/2 - Data Types & Operators/fix_the_quote.py
+++ Lessons/Introduction/2 - Data Types & Operators/fix_the_quote.py
@@ -6,4 +6,6 @@ First run it with Test Run to view the error message.
 Then resolve the problem so that the quote (from Henry Ford) is correctly assigned to the variable ford_quote.
 """
 # TODO: Fix this string!
-ford_quote = 'Whether you think you can, or you think you can't--you're right.'
+ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
+
+print(ford_quote)
