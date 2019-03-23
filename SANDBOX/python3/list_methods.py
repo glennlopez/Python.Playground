1eb475b15704c96e8910e420d786c0d40043f75c
Glenn Lopez <glennlopez@gmail.com>
2019-03-23 10:43:10 -0500

    added practice to sandbox

---
+++ SANDBOX/python3/list_methods.py
@@ -0,0 +1,15 @@
+
+# python list
+my_mix_list = ["Glenn", "Lopez", 1987, 3, 24, "A0123DEF"]
+
+my_string_list_1 = ["Glenn", "Lopez"]
+my_string_list_2 = ["ABC", "abc"]
+my_string_list_3 = ["abc", "ABC"]
+my_string_list_4 = ["AB", "ABC"]
+
+my_int_list_1 = [1, 34, 50, 12, 6, 5]
+
+
+print("String Length: " + str(len(my_mix_list)))
+print("Max(): " + str(max(my_string_list_4)))
+
