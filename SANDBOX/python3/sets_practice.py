395520a85f724a252dd4d37ca0086cf826a8aded
Glenn Lopez <glennlopez@gmail.com>
2019-03-25 22:30:17 -0500

    udpate

---
+++ SANDBOX/python3/sets_practice.py
@@ -0,0 +1,8 @@
+numbers = [8, 1, 2, 3, 1, 2, 3, 5, 8, 0, 9]
+my_set = set(numbers)
+print(my_set)
+print(82 in my_set)
+
+names = ["Glenn", "Lopez", "Glenn", "Gonazalez", "Burito"]
+my_name_set = set(names)
+print(my_name_set)
