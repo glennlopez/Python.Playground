12a511c6a7c30cbf906a1dd71dade533cfb4dd89
Glenn Lopez <glennlopez@gmail.com>
2019-03-27 11:51:18 -0500

    practice print formatting

---
+++ SANDBOX/python3/print_practice.py
@@ -0,0 +1,7 @@
+name = "Glenn"
+age = 12
+dob = 1977
+
+print("Dob: " + str(dob))
+print("INFO \n Name: {}\n Age: {}\n Year:{}".format(name, age, dob))
+print(f"Name: {name}, Age: {age}, Dob: {dob}")
