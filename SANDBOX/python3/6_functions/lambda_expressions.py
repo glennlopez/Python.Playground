e0b74ae1b93d0a5d3618e29ca34c19cbec3a11e1
Glenn Lopez <glennlopez@gmail.com>
2019-09-17 20:52:39 -0500

    practicing python

--- SANDBOX/python3/6_functions/lambda_expressions.py
+++ SANDBOX/python3/6_functions/lambda_expressions.py
@@ -11,9 +11,25 @@ def square_area(l, h):
 
 # lambda equivalent
 sqr_area = lambda l, h: l * h
-full_name = lambda fn = input("First name: "), ln = input("Last Name: "): print('Hello {} {}'.format(fn, ln))
+#full_name = lambda fn = input("First name: "), ln = input("Last Name: "): print('Hello {} {}'.format(fn, ln))
 
 print(square_area(3, 3))
 print(sqr_area(3, 3))
-full_name()
+#full_name()
+
+def add_10(num):
+    '''
+    Adds 10 to a number
+    :param num:
+    :return:
+    '''
+    return num + 10
+
+test = lambda x, y: x - y
+
+
+print(add_10(test(10,5)))
+
+
+
 
